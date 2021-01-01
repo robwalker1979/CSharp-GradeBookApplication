e88a6e895444c3ad5ceac21963df82b6dca9e0d8
Rob Walker <rob@walkernet.org.uk>
2021-01-01 20:21:34 +0000

    Added ranked and standard grade books

---
+++ GradeBook/Enums/GradeBookType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.Enums
+{
+    public enum GradeBookType
+    {
+        Standard,
+        Ranked,
+        ESNU
+    }
+}
