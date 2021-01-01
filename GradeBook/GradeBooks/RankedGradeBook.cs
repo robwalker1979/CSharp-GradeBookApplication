a3f09709be4a5146b48b1399726448602c7f6128
Rob Walker <rob@walkernet.org.uk>
2021-01-01 21:27:29 +0000

    Ranked-grading students

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 
@@ -9,6 +10,62 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             this.Type = Enums.GradeBookType.Ranked;
+        }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students?.Count < 5)
+            {
+                throw new InvalidOperationException(
+                    "There are less than 5 students. Ranked-grading requires a minimum of 5 students to work.");
+            }
+
+            var bandSize = this.CalculateBandSize();
+
+            var aGradeMin = GetMinAverageGradePerBand(bandSize, 1);
+            var bGradeMin = GetMinAverageGradePerBand(bandSize, 2);
+            var cGradeMin = GetMinAverageGradePerBand(bandSize, 3);
+            var dGradeMin = GetMinAverageGradePerBand(bandSize, 4);
+            var eGradeMin = GetMinAverageGradePerBand(bandSize, 5);
+
+            if (averageGrade >= aGradeMin)
+            {
+                return 'A';
+            }
+            else if (averageGrade >= bGradeMin)
+            {
+                return 'B';
+            }
+            else if (averageGrade >= cGradeMin)
+            {
+                return 'C';
+            }
+            else if (averageGrade >= dGradeMin)
+            {
+                return 'D';
+            }
+            else if (averageGrade >= eGradeMin)
+            {
+                return 'E';
+            }
+            else 
+            {
+                return 'F';
+            }
+        }
+
+        private double GetMinAverageGradePerBand(int bandSize, int bandNumber) 
+        {
+            return Students.OrderByDescending(s => s.AverageGrade)
+                                    .Skip((bandSize * bandNumber) - 1)
+                                    .Take(1)
+                                    .Single()
+                                    .AverageGrade;
+        }
+
+        private int CalculateBandSize()
+        {
+            var numberOfStudents = this.Students?.Count ?? 0;
+            return Convert.ToInt32(Math.Floor(numberOfStudents * 0.2));
         }
     }
 }
