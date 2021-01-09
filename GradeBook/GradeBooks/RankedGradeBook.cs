8da93d871d9a6c54bbf34b70e52e2e1bb2cf6acd
Rob Walker <rob@walkernet.org.uk>
2021-01-09 12:30:35 +0000

    override-statistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -63,5 +63,27 @@ namespace GradeBook.GradeBooks
             var numberOfStudents = this.Students?.Count ?? 0;
             return Convert.ToInt32(Math.Ceiling(numberOfStudents * 0.2));
         }
+
+        public override void CalculateStatistics()
+        {
+            if (this.Students?.Count() < 5) 
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (this.Students?.Count() < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+
+            base.CalculateStudentStatistics(name);
+        }
     }
 }
